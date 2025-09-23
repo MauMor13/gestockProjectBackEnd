ac8027b3dfdb55ce667fd77c5cb636954e3fa55e
Mauricio Mores <mauri.f.mores@gmail.com>
2025-09-22 23:39:00 -0300

    feat: Add CompanySchema to the data model

---
+++ src/dao/mongo/schema/companySchema.ts
@@ -0,0 +1,10 @@
+import {Schema} from 'mongoose';
+import type {ICompany} from '../../models/ICompany.js';
+
+export const CompanySchema = new Schema<ICompany>({
+    id: {type: String, required: true, unique: true},
+    name: {type: String, required: true},
+    emailCompany: {type: String, required: true, unique: true},
+    dataRegister: {type: Date, required: true, default: Date.now},
+    active: {type: Boolean, required: true, default: false}
+})
